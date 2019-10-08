0d9078f6fa58de1896540bb7209b7fd5e2a0428d
Osorio Neto <osor1o@hotmail.com>
2019-10-08 00:09:50 -0300

    Changed Message

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const electron = require("electron");
 const app = electron.app;
 
 app.on('ready', () => {
-    console.log('A aplicação Electron foi criado e pronta para interação');
+    console.log('Initialized Electron APP');
 });
