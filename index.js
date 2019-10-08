96c30a2d8132dee1e3bb7795eb77dcef08d70993
osor1o <osor1o@hotmail.com>
2019-10-08 03:46:51 -0300

    added index.html and mainWindow

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const electron = require("electron");
 
-const app = electron.app;
+const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
-    console.log('Initialized Electron APP');
+    const mainWindow = new BrowserWindow({})
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
 });
